7094b55603db3006b26f840778edc77e5e0cf11b
Yuta Nakamura <nakamurayuta0204@gmail.com>
2025-03-01 22:54:10 +0900

    Update css

--- components/Badge.vue
+++ components/Badge.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="absolute top-4 right-4 bg-red-400 text-white px-4 py-1 rounded-md !text-xl !font-bold"
+    class="absolute top-7 right-7 bg-red-400 text-white px-4 py-1 rounded-md !text-xl !font-bold"
   >
     <slot></slot>
   </div>
